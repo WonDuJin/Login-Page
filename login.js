5658710e7fe968aeb64e610e8dd274bdb5f50b78
WonDuJin <windyjan1@naver.com>
2022-09-26 15:07:24 +0900

    아이디 중복처리 연습

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 const USER_ID = "userid";
 const USER_PW = "userpw";
+
 function login() {
   const id = document.querySelector("#id");
   const pw = document.querySelector("#pw");
@@ -27,16 +28,20 @@ function sign() {
   const id = document.querySelector("#id");
   const pw = document.querySelector("#pw");
   const confirm_pw = document.querySelector("#confirm_pw");
+  const userid = id.value;
+  const userpw = pw.value;
 
   if (id.value === "" || pw.value === "" || confirm_pw.value === "") {
     alert("아이디 혹은 패스워드를 입력해주세요");
   } else if (pw.value !== confirm_pw.value) {
     alert("비밀번호가 동일하지 않습니다.");
   } else {
-    const userid = id.value;
-    const userpw = pw.value;
-    sessionStorage.setItem(USER_ID, userid);
-    sessionStorage.setItem(USER_PW, userpw);
+    sessionStorage.setItem(userid, userpw);
+    try {
+      sessionstorage.getItem(userid) != null;
+    } finally {
+      alert("이미 등록된 아이디 입니다.");
+    }
     alert("회원가입 성공!");
     location.href = "login.html";
   }
