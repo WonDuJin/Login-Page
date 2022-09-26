ba584ada114c9c4690f0a45cbeaec813dba56c13
WonDuJin <windyjan1@naver.com>
2022-09-26 12:16:58 +0900

    연습

--- login.js
+++ login.js
@@ -1,9 +1,11 @@
 function login() {
   const id = document.querySelector("#id");
   const pw = document.querySelector("#pw");
+  const userID = sessionStorage.getItem(USER_ID);
+  const userPW = sessionStorage.getItem(USER_PW);
   if (id.value === "" || pw.value === "") {
     alert("아이디 혹은 비밀번호를 입력해주세요.");
-  } else if (id.value !== "원두진" || pw.value !== "1234") {
+  } else if (id.value !== userID.value || pw.value !== userPW.value) {
     alert("아이디 혹은 비밀번호를 확인해주세요");
   } else {
     location.href = "mainpage.html";
@@ -18,12 +20,19 @@ function sign() {
   const id = document.querySelector("#id");
   const pw = document.querySelector("#pw");
   const confirm_pw = document.querySelector("#confirm_pw");
+  const USER_ID = "userid";
+  const USER_PW = "userpw";
 
   if (id.value === "" || pw.value === "" || confirm_pw.value === "") {
     alert("아이디 혹은 패스워드를 입력해주세요");
   } else if (pw.value !== confirm_pw.value) {
     alert("비밀번호가 동일하지 않습니다.");
   } else {
+    const userid = id.value;
+    const userpw = pw.value;
+    sessionStorage.setItem(USER_ID, userid);
+    sessionStorage.setItem(USER_PW, userpw);
+    alert("회원가입 성공!");
     location.href = "login.html";
   }
 }
--- sign.js
+++ sign.js
@@ -2,18 +2,14 @@ function sign() {
   const id = document.querySelector("#id");
   const pw = document.querySelector("#pw");
   const confirm_pw = document.querySelector("#confirm_pw");
-  const USER_ID = "userid";
-  const USER_PW = "userpw";
 
   if (id.value === "" || pw.value === "" || confirm_pw.value === "") {
     alert("아이디 혹은 패스워드를 입력해주세요");
-  } else if (pw.value !== confirm_pw) {
+  } else if (pw.value !== confirm_pw.value) {
     alert("비밀번호가 동일하지 않습니다.");
   } else {
-    const userid = id.value;
-    const userpw = pw.value;
-    sessionStorage.setItem(USER_ID, userid);
-    sessionStorage.setItem(USER_PW, userpw);
+    sessionStorage.setItem("userid", "id.value");
+    sessionStorage.setItem("userpw", "pw.value");
     alert("회원가입 성공!");
     location.href = "login.html";
   }
