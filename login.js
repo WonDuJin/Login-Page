5c308ee39ee250086494ea65813b199012351873
WonDuJin <windyjan1@naver.com>
2022-09-26 12:34:55 +0900

    로그인페이지(세션스토리지)

--- login.js
+++ login.js
@@ -1,13 +1,20 @@
+const USER_ID = "userid";
+const USER_PW = "userpw";
 function login() {
   const id = document.querySelector("#id");
   const pw = document.querySelector("#pw");
-  const userID = sessionStorage.getItem(USER_ID);
-  const userPW = sessionStorage.getItem(USER_PW);
+
   if (id.value === "" || pw.value === "") {
     alert("아이디 혹은 비밀번호를 입력해주세요.");
-  } else if (id.value !== userID.value || pw.value !== userPW.value) {
+  } else if (
+    id.value !== sessionStorage.getItem(USER_ID) ||
+    pw.value !== sessionStorage.getItem(USER_PW)
+  ) {
     alert("아이디 혹은 비밀번호를 확인해주세요");
-  } else {
+  } else if (
+    id.value === sessionStorage.getItem(USER_ID) &&
+    pw.value === sessionStorage.getItem(USER_PW)
+  ) {
     location.href = "mainpage.html";
   }
 }
@@ -20,8 +27,6 @@ function sign() {
   const id = document.querySelector("#id");
   const pw = document.querySelector("#pw");
   const confirm_pw = document.querySelector("#confirm_pw");
-  const USER_ID = "userid";
-  const USER_PW = "userpw";
 
   if (id.value === "" || pw.value === "" || confirm_pw.value === "") {
     alert("아이디 혹은 패스워드를 입력해주세요");
